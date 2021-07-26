953ada34288f006af9dc75bcf36ef597efe1b42e
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2021-07-26 21:33:44 +0530

    complete

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import requests
-# import os
+
 from datetime import datetime
 
-api_key = '828d88ace9c9dc10fdbdb1c22909b4d9'
-location = input("Enter the city name: ")
+apiKey = '828d88ace9c9dc10fdbdb1c22909b4d9'
+location = input("Enter city name: ")
 
-complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + api_key
-api_link = requests.get(complete_api_link)
-api_data = api_link.json()
 
-# create variables to store and display data
-temp_city = ((api_data['main']['temp']) - 273.15)
-weather_desc = api_data['weather'][0]['description']
-hmdt = api_data['main']['humidity']
-wind_spd = api_data['wind']['speed']
-date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey)
+data = response.json()
+
+
+temp = ((data['main']['temp']) - 273.15)
+dis = data['weather'][0]['description']
+h = data['main']['humidity']
+wind = data['wind']['speed']
+date = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 
 print("-------------------------------------------------------------")
-print("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print("Weather Stats for - {}  || {}".format(location.upper(), date))
 print("-------------------------------------------------------------")
 
-print("Current temperature is: {:.2f} deg C".format(temp_city))
-print("Current weather desc  :", weather_desc)
-print("Current Humidity      :", hmdt, '%')
-print("Current wind speed    :", wind_spd, 'kmph')
+print("Current temperature is: {:.2f} deg C".format(temp))
+print("Current weather desc  :", dis)
+print("Current Humidity      :", h, '%')
+print("Current wind speed    :", wind, 'kmph')
 
 print("====================================================")
 
-# making a list so that i can print the info to a txt
-txtlist = [temp_city, weather_desc, hmdt, wind_spd, date_time]
-# using open() buit-in function to write to a text file
-with open("textfile.txt", mode='w', encoding='utf-8') as f:
-    # encoding = utf-8 for linux and cp1252 for win
+
+txtlist = [temp, dis, h, wind, date]
+
+with open("output.txt", mode='w', encoding='utf-8') as f:
+
     f.write("------------------------------------------------------------- \n ")
-    f.write("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+    f.write("Weather Stats for - {}  || {}".format(location.upper(), date))
     f.write("\n ------------------------------------------------------------- \n")
     f.write("Current temperature is: {:.2f} deg C\n".format(txtlist[0]))
 
